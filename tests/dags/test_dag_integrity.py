0a305ac0f047564ac7871d3edd683db6eaae96f6
Bas Harenslak <basharenslak@gmail.com>
2019-10-27 22:15:55 +0100

    DAG integrity test fixes

--- tests/dags/test_dag_integrity.py
+++ tests/dags/test_dag_integrity.py
@@ -1,10 +1,11 @@
-"""Test integrity of dags."""
+"""Test integrity of DAGs."""
+
 import glob
-import importlib
+import importlib.util
 import os
 
 import pytest
-from airflow import models as af_models
+from airflow.models import DAG
 
 DAG_PATH = os.path.join(os.path.dirname(__file__), "..", "..", "dags", "**/*.py")
 DAG_FILES = glob.glob(DAG_PATH)
@@ -12,15 +13,15 @@ DAG_FILES = glob.glob(DAG_PATH)
 
 @pytest.mark.parametrize("dag_file", DAG_FILES)
 def test_dag_integrity(dag_file):
-    print(f"dagfile = {dag_file}")
-    """Import dag files and check for DAG."""
+    """Import DAG files and check for DAG."""
+    print(f"DAG file = {dag_file}")
     module_name, _ = os.path.splitext(dag_file)
     module_path = os.path.join(DAG_PATH, dag_file)
     mod_spec = importlib.util.spec_from_file_location(module_name, module_path)
     module = importlib.util.module_from_spec(mod_spec)
     mod_spec.loader.exec_module(module)
 
-    dag_objects = [var for var in vars(module).values() if isinstance(var, af_models.DAG)]
+    dag_objects = [var for var in vars(module).values() if isinstance(var, DAG)]
     assert dag_objects
 
     for dag in dag_objects:
