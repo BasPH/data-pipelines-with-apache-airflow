39b12a8f8b7a07b4b034225d913fac98540dd556
Bas Harenslak <basharenslak@gmail.com>
2019-12-31 15:04:25 +0100

    Create installable package

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="airflowbook",
+    version="0.1",
+    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "src"},
+    # install_requires=[...],
+    extras_require={"dev": ["pytest_docker_tools~=0.2.0"]},
+    python_requires=">=3.6.*",
+)
