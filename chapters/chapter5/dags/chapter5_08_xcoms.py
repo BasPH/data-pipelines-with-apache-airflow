4d0ed55deaf69a51138f6c31dfdcd9dc2f009752
Julian de Ruiter <julianderuiter@godatadriven.com>
2020-07-29 22:45:19 +0200

    Add xcom example.

--- chapters/chapter5/dags/chapter5_08_xcoms.py
+++ chapters/chapter5/dags/chapter5_08_xcoms.py
@@ -34,9 +34,19 @@ with DAG(
 
     join_datasets = DummyOperator(task_id="join_datasets")
 
+    def _train_model(**context):
+        model_id = str(uuid.uuid4())
+        context["task_instance"].xcom_push(key="return_value", value=model_id)
+
     train_model = PythonOperator(
         task_id="train_model", python_callable=_train_model, provide_context=True,
     )
+
+    def _deploy_model(**context):
+        model_id = context["task_instance"].xcom_pull(
+            task_ids="train_model", key="model_id"
+        )
+        print(f"Deploying model {model_id}")
 
     deploy_model = PythonOperator(
         task_id="deploy_model", python_callable=_deploy_model, provide_context=True,
