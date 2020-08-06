79a11346806be6c155b9b7d9fbbf969bf81aabe2
Julian de Ruiter <julianderuiter@godatadriven.com>
2020-08-06 21:46:53 +0200

    Add makefile + readme for AWS/Azure.

--- chapters/chapter13/aws/dags/custom/ratings.py
+++ chapters/chapter13/aws/dags/custom/ratings.py
@@ -9,7 +9,7 @@ def fetch_ratings(year, month):
     """Fetches ratings for a given month/year."""
 
     try:
-        ratings = pd.read_csv(DATA_DIR / "partitioned" / str(year) /f"{month}.csv")
+        ratings = pd.read_csv(DATA_DIR / "ratings" / str(year) /f"{month:02d}.csv")
     except FileNotFoundError:
         ratings = pd.DataFrame.from_records(
             [],
