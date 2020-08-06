b39163ea1af7653676fc6260ec5509af677a5f2a
Julian de Ruiter <julianderuiter@godatadriven.com>
2020-08-06 20:32:30 +0200

    Fix flake8 issue.

--- chapters/chapter3/api/app.py
+++ chapters/chapter3/api/app.py
@@ -12,7 +12,7 @@ def _generate_events(end_date):
     """Generates a fake dataset with events for 30 days before end date."""
 
     events = pd.concat([
-        _generate_events_for_day(date=end_date - timedelta(days=(30-i)))
+        _generate_events_for_day(date=end_date - timedelta(days=(30 - i)))
         for i in range(30)
     ], axis=0)
 
