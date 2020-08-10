6599df947410ff2c426fb71dd347349f7d53cc61
Bas Harenslak <basharenslak@gmail.com>
2020-08-10 22:24:05 +0200

    Fix Black formatting

--- chapters/chapter14/ldap/webserver_config.py
+++ chapters/chapter14/ldap/webserver_config.py
@@ -18,8 +18,10 @@
 # under the License.
 """Default configuration for the Airflow webserver"""
 import os
+
 # from flask_appbuilder.security.manager import AUTH_DB
 from flask_appbuilder.security.manager import AUTH_LDAP
+
 # from flask_appbuilder.security.manager import AUTH_OAUTH
 # from flask_appbuilder.security.manager import AUTH_OID
 # from flask_appbuilder.security.manager import AUTH_REMOTE_USER
@@ -29,7 +31,7 @@ from airflow.configuration import conf
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # The SQLAlchemy connection string.
-SQLALCHEMY_DATABASE_URI = conf.get('core', 'SQL_ALCHEMY_CONN')
+SQLALCHEMY_DATABASE_URI = conf.get("core", "SQL_ALCHEMY_CONN")
 
 # Flask-WTF flag for CSRF
 CSRF_ENABLED = True
