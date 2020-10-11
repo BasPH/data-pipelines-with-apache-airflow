0df3941854783a540178818bc186c25bd2d1be20
Bas Harenslak <basharenslak@gmail.com>
2020-10-11 19:32:23 +0200

    Refactor Chapter 2 with TE feedback

---
+++ chapters/chapter02/dags/listing_2_3.py
@@ -0,0 +1,4 @@
+import airflow
+from airflow import DAG
+
+dag = DAG(dag_id="listing_2_3", start_date=airflow.utils.dates.days_ago(14), schedule_interval=None)
---
+++ chapters/chapter02/dags/listing_2_4.py
@@ -0,0 +1,11 @@
+import airflow
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+
+dag = DAG(dag_id="listing_2_4", start_date=airflow.utils.dates.days_ago(14), schedule_interval=None)
+
+download_launches = BashOperator(
+    task_id="download_launches",
+    bash_command="curl -o /tmp/launches.json 'https://launchlibrary.net/1.4/launch?next=5&mode=verbose'",
+    dag=dag,
+)
