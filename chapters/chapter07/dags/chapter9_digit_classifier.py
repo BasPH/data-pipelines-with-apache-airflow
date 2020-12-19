e99cacf9fcd16b52dd3caa329df161d37e9242d1
Julian de Ruiter <julianderuiter@godatadriven.com>
2020-12-19 16:22:41 +0100

    Rename remaining old chapters to new numbers.

--- chapters/chapter07/dags/chapter9_digit_classifier.py
+++ chapters/chapter07/dags/chapter9_digit_classifier.py
@@ -7,8 +7,12 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from airflow.providers.amazon.aws.operators.s3_copy_object import S3CopyObjectOperator
-from airflow.providers.amazon.aws.operators.sagemaker_endpoint import SageMakerEndpointOperator
-from airflow.providers.amazon.aws.operators.sagemaker_training import SageMakerTrainingOperator
+from airflow.providers.amazon.aws.operators.sagemaker_endpoint import (
+    SageMakerEndpointOperator,
+)
+from airflow.providers.amazon.aws.operators.sagemaker_training import (
+    SageMakerTrainingOperator,
+)
 from sagemaker.amazon.common import write_numpy_to_dense_tensor
 
 dag = DAG(
