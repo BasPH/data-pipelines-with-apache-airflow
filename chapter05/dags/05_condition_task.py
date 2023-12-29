995ac1bdf6a81c7e70b8f13701651925e325caf4
Kris Geusebroek <krisgeus@gmail.com>
2023-12-29 16:34:25 +0100

    Incorporate PR#70

--- chapter05/dags/05_condition_task.py
+++ chapter05/dags/05_condition_task.py
@@ -8,7 +8,7 @@ ERP_CHANGE_DATE = airflow.utils.dates.days_ago(1)
 
 
 def _pick_erp_system(**context):
-    if context["execution_date"] < airflow.utils.dates.days_ago(1):
+    if context["execution_date"] < ERP_CHANGE_DATE:
         return "fetch_sales_old"
     else:
         return "fetch_sales_new"
